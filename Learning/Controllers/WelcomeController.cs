9617ed3cde7c824e6c3834a7d2449b1268cbcc60
Niranjan Gangula <gniru.infy@gmail.com>
2016-01-28 22:23:43 +1100

    Learning formcollection data

---
+++ Learning/Controllers/WelcomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Learning.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        // GET: Welcome
+        public ActionResult Index(FormCollection data)
+        {
+            var firstname = data["fname_name"];
+            var lastname = data["lname_name"];
+            Response.Write("Full name is" + firstname + " " + lastname);
+            return View();
+        }
+    }
+}
