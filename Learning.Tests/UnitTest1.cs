486d26d49305c3e5ae9e59c5798e92aaf39525cb
Niranjan Gangula <gniru.infy@gmail.com>
2016-01-28 21:49:47 +1100

    Initial Check In

---
+++ Learning.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Learning.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
